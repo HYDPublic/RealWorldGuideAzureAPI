7a93b90ece2d0e2de6873fb2c1f95ee53df65f7e
Heather Downing <quorralyne@gmail.com>
2019-03-05 20:58:39 -0600

    Added Data project

---
+++ RealWorldGuide.Data/Guide.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RealWorldGuide.Data
+{
+    class Guide
+    {
+        public int Id { get; set; }
+
+        public string GuideName { get; set; }
+        public string Author { get; set; }
+        public DateTime LastUpdated { get; set; }
+    }
+}
